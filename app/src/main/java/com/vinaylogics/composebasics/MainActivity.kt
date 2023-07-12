c56c4600cb579ed5924e0918cd9c230efd0991fd
Vinayagam <vinayagam.d.ganesh@gmail.com>
2023-07-12 05:58:39 +0530

    Added the tween animation

--- app/src/main/java/com/vinaylogics/composebasics/MainActivity.kt
+++ app/src/main/java/com/vinaylogics/composebasics/MainActivity.kt
@@ -3,8 +3,26 @@ package com.vinaylogics.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Easing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlin.time.Duration.Companion.seconds
 
 class MainActivity : ComponentActivity() {
 
@@ -14,7 +32,27 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var sizeState by remember {
+                mutableStateOf(200.dp)
+            }
+            val size by animateDpAsState(
+                targetValue = sizeState,
+                tween(
+                    durationMillis = 3000,
+                    delayMillis = 300,
+                    easing = LinearOutSlowInEasing)
+                )
+            Box(modifier = Modifier
+                .size(size)
+                .background(Color.Red),
+                contentAlignment = Alignment.Center) {
 
+                Button(onClick = {
+                    sizeState += 50.dp
+                }) {
+                    Text("Increase Size")
+                }
+            }
         }
 
     }

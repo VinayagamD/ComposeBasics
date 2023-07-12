ee8289e9eea9bde8b3023a48bb0f32510643345c
Vinayagam <vinayagam.d.ganesh@gmail.com>
2023-07-12 06:26:51 +0530

    Added the infinite transition animation

--- SimpleAnimation/src/main/java/com/vinaylogics/simpleanimation/theme/MyAnimations.kt
+++ SimpleAnimation/src/main/java/com/vinaylogics/simpleanimation/theme/MyAnimations.kt
@@ -1,11 +1,16 @@
 package com.vinaylogics.simpleanimation.theme
 
+import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.FastOutLinearInEasing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.RepeatMode
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.keyframes
+import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
@@ -100,3 +105,37 @@ fun GetKeyFramesAnimation(){
         }
     }
 }
+
+@Composable
+fun GetInfiniteTransitionAnimation(){
+    var sizeState by remember {
+        mutableStateOf(200.dp)
+    }
+    val size by animateDpAsState(
+        targetValue = sizeState,
+        tween(
+            durationMillis = 1000
+        )
+    )
+    val infiniteTransition = rememberInfiniteTransition()
+    val color by infiniteTransition.animateColor(
+        initialValue = Color.Red,
+        targetValue = Color.Green,
+        animationSpec =  infiniteRepeatable(
+            tween(durationMillis = 2000),
+            repeatMode = RepeatMode.Reverse
+        )
+    )
+    Box(modifier = Modifier
+        .size(size)
+        .background(color),
+        contentAlignment = Alignment.Center) {
+
+        Button(onClick = {
+            sizeState += 50.dp
+        }) {
+            Text("Increase Size")
+        }
+    }
+}
+

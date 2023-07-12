ccff4def824fa805b0b64c9e60c72948c90e90d0
Vinayagam <vinayagam.d.ganesh@gmail.com>
2023-07-12 11:00:36 +0530

    Implemented Animated CircularProgressBar

--- app/src/main/java/com/vinaylogics/composebasics/MainActivity.kt
+++ app/src/main/java/com/vinaylogics/composebasics/MainActivity.kt
@@ -3,8 +3,13 @@ package com.vinaylogics.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.vinaylogics.animatecircularprogressbar.CircularProgressBar
 import com.vinaylogics.simpleanimation.GetInfiniteTransitionAnimation
 
 class MainActivity : ComponentActivity() {
@@ -15,7 +20,14 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           GetInfiniteTransitionAnimation()
+           Box(
+               contentAlignment = Alignment.Center,
+               modifier = Modifier.fillMaxSize()
+           ) {
+
+               CircularProgressBar(percentage = 0.8f, number = 100)
+
+           }
         }
 
     }

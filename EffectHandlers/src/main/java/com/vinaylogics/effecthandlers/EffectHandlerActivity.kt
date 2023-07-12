6d9c0e38b3987c8544a130f4b09632fc0838426f
Vinayagam <vinayagam.d.ganesh@gmail.com>
2023-07-12 05:40:31 +0530

    Completed the Effect Handler

---
+++ EffectHandlers/src/main/java/com/vinaylogics/effecthandlers/EffectHandlerActivity.kt
@@ -0,0 +1,49 @@
+package com.vinaylogics.effecthandlers
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.tooling.preview.Preview
+import com.vinaylogics.effecthandlers.ui.theme.ComposeBasicsTheme
+import kotlinx.coroutines.delay
+import kotlin.time.Duration.Companion.seconds
+
+class MainActivity : ComponentActivity() {
+
+
+    private var i = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+           var text by remember {
+               mutableStateOf("")
+           }
+
+            ComposeBasicsTheme {
+                LaunchedEffect(key1 = text ) {
+                    delay(1.seconds)
+                    println("The text is $text")
+                }
+                
+            }
+        }
+
+    }
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun ShowImageCard(){
+
+}
+

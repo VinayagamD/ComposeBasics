e63c80113c07ca139b720203d9424f56deff0b09
Vinayagam <vinayagam.d.ganesh@gmail.com>
2023-07-13 20:33:54 +0530

    Implemented the animation 3d drop down complete

--- app/src/main/java/com/vinaylogics/composebasics/MainActivity.kt
+++ app/src/main/java/com/vinaylogics/composebasics/MainActivity.kt
@@ -3,14 +3,20 @@ package com.vinaylogics.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.vinaylogics.a3danimationdropdown.DropDown
 import com.vinaylogics.mediationui.ui.home.HomeScreen
 import com.vinaylogics.mytimer.Timer
 
@@ -26,15 +32,17 @@ class MainActivity : ComponentActivity() {
                 color = Color(0xFF101010),
                 modifier = Modifier.fillMaxSize()
             ) {
-                Box(
-                    contentAlignment = Alignment.Center
-                ){
-                    Timer(
-                        totalTime = 100* 1000L,
-                        handleColor = Color.Green,
-                        activeBarColor = Color(0xFF37B900),
-                        inactiveBarColor = Color.DarkGray,
-                        modifier = Modifier.size(200.dp)
+
+                DropDown(
+                    text = "Hello World!",
+                    modifier = Modifier.padding(15.dp)
+                ) {
+                    Text(
+                        text = "This is now revealed!",
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(100.dp)
+                            .background(Color.Green)
                     )
                 }
 

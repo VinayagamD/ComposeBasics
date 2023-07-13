e4f4733d755331177fd36e2497839366b4568a0a
Vinayagam <vinayagam.d.ganesh@gmail.com>
2023-07-13 10:35:41 +0530

    Implemented the timer complete

--- app/src/main/java/com/vinaylogics/composebasics/MainActivity.kt
+++ app/src/main/java/com/vinaylogics/composebasics/MainActivity.kt
@@ -3,7 +3,16 @@ package com.vinaylogics.composebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Surface
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.vinaylogics.mediationui.ui.home.HomeScreen
+import com.vinaylogics.mytimer.Timer
 
 class MainActivity : ComponentActivity() {
 
@@ -13,7 +22,23 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            HomeScreen()
+            Surface(
+                color = Color(0xFF101010),
+                modifier = Modifier.fillMaxSize()
+            ) {
+                Box(
+                    contentAlignment = Alignment.Center
+                ){
+                    Timer(
+                        totalTime = 100* 1000L,
+                        handleColor = Color.Green,
+                        activeBarColor = Color(0xFF37B900),
+                        inactiveBarColor = Color.DarkGray,
+                        modifier = Modifier.size(200.dp)
+                    )
+                }
+
+            }
         }
 
     }
